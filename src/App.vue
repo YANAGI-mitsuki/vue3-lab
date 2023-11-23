a52e70836a4c089c8db0777dce2c37809447e1e6
Mitsuki Yanagi <iganaymjs2@gmail.com>
2023-11-23 16:46:30 +0900

    change src/App.vue/script/item

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,40 @@
 <script setup>
 import { ref } from 'vue'
-const item = ref({
+const item = ref([
+{
     id: 1,
-    name: 'アボカドディップバケット',
+    name: 'アボカドソースバケット',
     description:
-      '刻んだ野菜をアボカドと混ぜてディップに。こんがり焼いたバゲットとお召し上がりください。',
-    price: 480,
+      '刻んだ野菜をアボカドと混ぜ、優しい味のソースに。こんがり焼いたバゲットとお召し上がりください。',
+    price: 320,
     image: '/images/item1.jpg'
-})
+},
+{
+    id: 2,
+    name: 'あの日夢見たホットケーキ',
+    description:
+      '子供のころに食べたかった、あのホットケーキを再現しました。素朴でどこか懐かしい味をどうぞ。',
+    price: 1180,
+    image: '/images/item2.jpg'
+},
+{
+    id: 3,
+    name: 'HOP WTR',
+    description:
+      'ロサンゼルス生まれのスパークリングウォーター。ノンカロリー、ノンアルコールの新感覚飲料です。',
+    price: 320,
+    image: '/images/item3.jpg'
+},
+{
+    id: 4,
+    name: 'チーズフレンチフライ',
+    description:
+      'イタリア産チーズをたっぷりかけたアツアツのフレンチフライ。みんな大好きな一品です。',
+    price: 670,
+    image: '/images/item4.jpg'
+}
+]
+  )
 </script>
 
 <template>
