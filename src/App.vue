ece6e73748ed95ed02a3512698b65b9001b6bd29
Mitsuki Yanagi <iganaymjs2@gmail.com>
2023-11-23 16:35:33 +0900

    change src/App.vue/style

--- src/App.vue
+++ src/App.vue
@@ -33,31 +33,85 @@ const item = ref({
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+body {
+  font-family: sans-serif;
+  margin: 0;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 
-.logo {
+#app {
+  width: 90%;
+  margin: 0 5%;
+  color: #242424;
+}
+
+.header {
+  display: flex;
+  align-content: center;
+  align-items: center;
+  margin-top: 40px;
+  margin-bottom: 40px;
+}
+
+.header > img {
+  width: 100px;
+  height: 100px;
+  margin-right: 20px;
+}
+
+.header > h1 {
+  font-size: 80px;
+  font-weight: bold;
+  line-height: 80px;
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+.main {
+  display: grid;
+  grid-template-columns: 3fr 3fr 3fr 3fr;
+  column-gap: 24px;
+  row-gap: 24px;
+}
+
+.item {
+  padding: 10px;
+  cursor: pointer;
+}
+
+.item:hover {
+  transition: 0.2s transform ease-out;
+  transform: scale(1.05);
+}
+
+.item > div.thumbnail > img {
+  width: 100%;
+  height: calc(100%);
+  object-fit: cover;
+}
+
+.item > div.description {
+  text-align: left;
+  margin-top: 20px;
+}
+
+.item > div.description > p {
+  margin-top: 0px;
+  margin-bottom: 0px;
+  font-size: 18px;
+  line-height: 25px;
+}
+
+.item > div.description > span {
   display: block;
-  margin: 0 auto 2rem;
+  margin-top: 10px;
+  font-size: 20px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.item > div.description > span > .price {
+  font-size: 28px;
+  font-weight: bold;
 }
 </style>
